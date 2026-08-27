561e0e4a418d9aa3704605da859582b4d3ad6b78
alessandrocardelli <38191355+alessandrocardelli@users.noreply.github.com>
2026-08-27 22:49:54 +0200

    feat: mount global settings menu

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'mathlive'
 import App from './App'
+import { AppMenu } from './components/AppMenu'
 import { PwaUpdatePrompt } from './components/PwaUpdatePrompt'
 import './styles.css'
 import './practiceModes.css'
@@ -13,6 +14,7 @@ import './pwaUpdatePrompt.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <AppMenu />
     <PwaUpdatePrompt />
   </React.StrictMode>,
 )
