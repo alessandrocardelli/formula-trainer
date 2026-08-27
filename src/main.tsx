8bcf64e39b63a2745d6b36b5f9d9be134d0e33d8
alessandrocardelli <38191355+alessandrocardelli@users.noreply.github.com>
2026-08-27 17:15:50 +0200

    feat: mount PWA update prompt

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'mathlive'
 import App from './App'
+import { PwaUpdatePrompt } from './components/PwaUpdatePrompt'
 import './styles.css'
 import './practiceModes.css'
 import './metadataFlow.css'
 import './backupControls.css'
+import './pwaUpdatePrompt.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <PwaUpdatePrompt />
   </React.StrictMode>,
 )
